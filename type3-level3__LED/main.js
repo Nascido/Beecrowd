fe210ce88db62a97efa22cd1897f905d9e745f5c
Nascido <rafael124715@gmail.com>
2023-05-02 10:07:56 -0300

    mod string

--- type3-level3__LED/main.js
+++ type3-level3__LED/main.js
@@ -4,8 +4,7 @@ var lines = input.trim().split('\n');
 
 let n = lines[0]; 
 let i;
-const ledsNumbers = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-                     [6, 2, 5, 5, 4, 5, 6, 4, 7, 6]] ;
+const ledsNumbers = [6, 2, 5, 5, 4, 5, 6, 4, 7, 6] ;
 
 for (i=1;i<=n;i++){
     const number = lines[i].toString();
@@ -16,8 +15,7 @@ for (i=1;i<=n;i++){
     for (j=0;j<size;j++){
 
         const digit = parseInt(number[j]);
-        const posi = ledsNumbers[0].indexOf(digit);
-        ledSum += parseInt(ledsNumbers[1][posi]);
+        ledSum += ledsNumbers[digit];
 
     }
     console.log(ledSum + " leds");
