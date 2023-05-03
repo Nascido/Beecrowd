9516718f0de42ebc4ce169483dc610047f6bf34b
Nascido <rafael124715@gmail.com>
2023-05-03 19:18:26 -0300

    Big Numbers Bug version

--- type3-level3__LED/main.js
+++ type3-level3__LED/main.js
@@ -3,20 +3,22 @@ var input = require('fs').readFileSync('inputs.txt', 'utf8');
 var lines = input.trim().split('\n');
 
 let n = lines[0]; 
+
+if (n>2000){n = 2000}
+
 let i;
 const ledsNumbers = [6, 2, 5, 5, 4, 5, 6, 4, 7, 6] ;
 
 for (i=1;i<=n;i++){
     const number = lines[i].toString();
-
     const size = number.length;
     let j, ledSum=0;
 
     for (j=0;j<size;j++){
-
         const digit = parseInt(number[j]);
         ledSum += ledsNumbers[digit];
 
     }
+    
     console.log(ledSum + " leds");
 }
