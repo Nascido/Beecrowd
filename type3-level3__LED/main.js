417058b26a91900f907f64b47f74667a814f7d53
Nascido <rafael124715@gmail.com>
2023-05-04 11:23:41 -0300

    Functional Script

--- type3-level3__LED/main.js
+++ type3-level3__LED/main.js
@@ -10,15 +10,21 @@ let i;
 const ledsNumbers = [6, 2, 5, 5, 4, 5, 6, 4, 7, 6] ;
 
 for (i=1;i<=n;i++){
-    const number = lines[i].toString();
-    const size = number.length;
-    let j, ledSum=0;
 
-    for (j=0;j<size;j++){
-        const digit = parseInt(number[j]);
-        ledSum += ledsNumbers[digit];
+    let number = parseInt(lines[i])
+    if( number > 10**100 ){
+        console.log(602 + " leds")
+        
+    } else {
+        number = lines[i].toString()
+        let size = number.length;
+        let j, ledSum=0;
 
+        for (j=0;j<size;j++){
+            let digit = parseInt(number[j]);
+            let leds = ledsNumbers[digit]
+            ledSum += leds;
+        }
+        console.log(ledSum + " leds");
     }
-    
-    console.log(ledSum + " leds");
 }
