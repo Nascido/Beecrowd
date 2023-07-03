309f0e45cdab5fd8612cd2f91992f029a5e03347
Rafael <rafaelnasci26@gmail.com>
2023-07-03 11:52:10 -0300

    reviw

--- type3-level3__Combinador/main.js
+++ type3-level3__Combinador/main.js
@@ -2,7 +2,5 @@ var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
 /**
- * Escreva a sua solução aqui
- * Code your solution here
- * Escriba su solución aquí
+Ficou uma merda
  */
